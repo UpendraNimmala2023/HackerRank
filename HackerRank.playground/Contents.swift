e8474ef324a2fb41115bc130eeeb2c98762159a3
UpendraNimmala2023 <135559739+UpendraNimmala2023@users.noreply.github.com>
2024-04-05 22:50:43 -0400

    Complted

--- HackerRank.playground/Contents.swift
+++ HackerRank.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+func countCamelCase(_ str:String) -> Int {
+   
+    guard str.count != 0 else {
+        return 0
+    }
+    var count = 1
+        for charector in str {
+            if charector.isUppercase {
+                count += 1
+            }
+        }
+    
+
+    
+    return count
+}
+
+print(countCamelCase("one"))
